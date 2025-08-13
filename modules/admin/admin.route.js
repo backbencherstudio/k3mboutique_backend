6c9745e7a1cf3daddbd557da9807bdd708ae3db2
Tohidul0 <tohidulalam967418@gmail.com>
2025-08-13 12:29:40 +0600

    play update done and also make match model and create match api make done

--- modules/admin/admin.route.js
+++ modules/admin/admin.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { verifyAdmin } = require("../../middleware/verifyAdmin");
-const { createNewPlayer } = require("./admin.controller");
+const { createNewPlayer, updatePlayer, getPlayer, createMatch,  } = require("./admin.controller");
 const { uploadProfileImage } = require("../../middleware/multer.config.single");
+const upload = require("../../middleware/multer.config.single");
 
 
 const route = require("express").Router();
@@ -9,4 +10,11 @@ const route = require("express").Router();
 
 
 route.post("/new-palyer-create",verifyAdmin,  createNewPlayer);
+route.put("/palyer-data-upadte/:playerId",verifyAdmin,upload.uploadProfileImage,  updatePlayer);
+route.get("/single-player/:playrId", getPlayer);
+
+
+
+
+route.post("/create-match",verifyAdmin, createMatch )
 module.exports = route;
